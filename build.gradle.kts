5948806476ebbd5987ea65147912445b7ffe308d
Raman Gupta <rocketraman@gmail.com>
2022-04-28 12:40:36 -0400

    Reproduce issue 19939

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  application
+}
+
+group = "com.rocketraman"
+version = "1.0"
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(11))
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "test-gradle-19939"
